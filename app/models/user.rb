02c15b28416301fb605e57a621fb586d524913d4
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-05 19:16:03 -0600

    Add github_repos instance method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
   validates :first_name, presence: true
+
   enum role: { default: 0, admin: 1 }
   has_secure_password
+
+  def github_repos
+   GithubService.new.repos(github_username, github_token)
+  end
 end
