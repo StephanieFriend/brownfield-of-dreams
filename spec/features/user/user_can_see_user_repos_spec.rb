ae3ad959c3fad862b3f9c0318c856181ce913b55
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-06 13:15:43 -0600

    Add test no github section on dashboard if missing credentails

--- spec/features/user/user_can_see_user_repos_spec.rb
+++ spec/features/user/user_can_see_user_repos_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'User' do
-  it 'user can sign in' do
+  it 'user can see repos if they have a token' do
     user = User.create(email: 'user@email.com', password: 'password', first_name:'Jim', role: 0, github_token: ENV['GITHUB_TOKEN'], github_username: ENV['GITHUB_USERNAME'])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -11,5 +11,14 @@ describe 'User' do
     within("section.github") do
       expect(page).to have_css(".repo", count: 5)
     end
+  end
+  
+  it 'user can not see repos if they do not have a token' do
+    user = User.create(email: 'user@email.com', password: 'password', first_name:'Jim', role: 0)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    expect(page).to_not have_content("Github")
   end
 end
