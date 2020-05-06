1398869154103edd94b2053ead6d8142a7089675
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-06 15:28:19 -0600

    Add github Followers method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,15 @@ class User < ApplicationRecord
   has_secure_password
 
   def github_repos(limit = nil)
-   json = GithubService.new.repos(github_username, github_token)
-   repo_objects = json.map {|repo_detail| Repo.new(repo_detail)}
-   limit ? repo_objects.take(limit) : repo_objects
+    json = GithubService.new.repos(github_username, github_token)
+    repo_objects = json.map {|repo_detail| Repo.new(repo_detail)}
+    limit ? repo_objects.take(limit) : repo_objects
+  end
+
+  def github_followers(limit = nil)
+    json = GithubService.new.followers(github_username, github_token)
+    followers = json.map {|user_json| GithubUser.new(user_json)}
+    limit ? followers.take(limit) : followers
   end
 
 end
