8c002bc265994115bf9191355aa38cdc207f67d9
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-09 16:10:57 -0600

    Create test for import admin playlist

--- spec/features/admin/admin_can_import_playlist_spec.rb
+++ spec/features/admin/admin_can_import_playlist_spec.rb
@@ -8,19 +8,18 @@ feature "An admin visiting /tutorials/new" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit "/admin/tutorials/new"
+    
+    click_link("Import Youtube Playlist")
 
-    click_on("Import YouTube Playlist")
+    fill_in "Playlist Id", with: "PL1cFDQFVTgspB01PHMDMM0U9Ckm7_mNa3"
 
-    fill_in "Playlist Id", with: "PL1cFDQFVTgsqO-7pdYHib9u1Cm9t_JsGP"
-
-    click_on("Create")
+    click_on("Import")
+    
 
     expect(current_path).to eq(admin_dashboard_path)
     expect(page).to have_content("Successfully created tutorial.")
-    click_link("View it here.")
-    expect(current_path).to eq(tutorial_path())
-
-    # figure out how to test if each video is being displayed!?
+    click_link("View it here")
+   
 
   end
 end
