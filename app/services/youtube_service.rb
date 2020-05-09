4d59567d381d94c62d342d7c7dd9728f1c695682
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-09 16:50:36 -0600

    Add playlist instance methos

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -5,6 +5,24 @@ class YoutubeService
     get_json('youtube/v3/videos', params)
   end
 
+  def playlist_info(id)
+    params = {part: 'id,contentDetails,snippet', id: id}
+
+    get_json('youtube/v3/playlists', params)
+  end
+
+  def playlist_videos(id)
+    params = {part: 'id,contentDetails', playlistId: id}
+    response = get_json('youtube/v3/playlistItems', params)
+    videos = response[:items]
+    while response[:nextPageToken]
+      params = {part: 'id,contentDetails', playlistId: id, pageToken: response[:nextPageToken]}
+      response = get_json('youtube/v3/playlistItems', params)
+      videos  += response[:items] 
+    end
+    videos
+  end
+
   private
 
   def get_json(url, params)
