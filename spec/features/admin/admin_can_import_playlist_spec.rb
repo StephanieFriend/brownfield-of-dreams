b3de3fbbea79a45dec4f6fbd9259bd582de62b43
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-09 16:50:03 -0600

    Add sad path testing for tutorial contoller

--- spec/features/admin/admin_can_import_playlist_spec.rb
+++ spec/features/admin/admin_can_import_playlist_spec.rb
@@ -10,16 +10,20 @@ feature "An admin visiting /tutorials/new" do
     visit "/admin/tutorials/new"
     
     click_link("Import Youtube Playlist")
-
-    fill_in "Playlist Id", with: "PL1cFDQFVTgspB01PHMDMM0U9Ckm7_mNa3"
-
+    fill_in "Playlist Id", with: "$$cash_money$$"
     click_on("Import")
+
+    expect(page).to have_content("Unable to create playlist")
+
+    visit "/admin/tutorials/new"
     
+    click_link("Import Youtube Playlist")
+    fill_in "Playlist Id", with: "PL1cFDQFVTgspB01PHMDMM0U9Ckm7_mNa3"
+    click_on("Import")
 
     expect(current_path).to eq(admin_dashboard_path)
     expect(page).to have_content("Successfully created tutorial.")
     click_link("View it here")
-   
-
   end
+
 end
