5c4d7cfc8490288ab648b4f919dbd0d87da6a070
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-11 00:24:48 -0600

    Write test for video class method list_bookmarks

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Video do
   it {should belong_to :tutorial}
   it {should have_many(:users).through(:user_videos)}
   it {should have_many(:user_videos).dependent(:destroy)}
+
+  describe 'class methods' do
+    it 'list_bookmarks' do
+      tutorial1= create(:tutorial, title: "How to Tie Your Shoes")
+      video1 = create(:video, title: "The Bunny Ears Technique", tutorial: tutorial1, bookmark: true)
+      tutorial2= create(:tutorial, title: "How to Wash Your Hands")
+      video2 = create(:video, title: "Lasting 20 Seconds", tutorial: tutorial2, bookmark: true)
+      video3 = create(:video, title: "What Soap To Use", tutorial: tutorial2, bookmark: true)
+
+      expect(Video.list_bookmarks.keys).to eq([tutorial1.title, tutorial2.title])
+      expect(Video.list_bookmarks.values).to eq([[video1], [video2, video3]])
+    end
+  end
 end
