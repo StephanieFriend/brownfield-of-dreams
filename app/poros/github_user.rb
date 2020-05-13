51efbacfce17e2d3b05822682952314212e50064
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-13 11:53:02 -0600

    Remove uid initialize and attr_reader

--- app/poros/github_user.rb
+++ app/poros/github_user.rb
@@ -1,7 +1,6 @@
 class GithubUser
-  attr_reader :name, :url, :avatar_url, :uid
+  attr_reader :name, :url, :avatar_url
   def initialize(user_details = {})
-    @uid = user_details[:id]
     @name = user_details[:login]
     @url = user_details[:url]
     @avatar = user_details[:avatar_url]
