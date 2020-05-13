7abcd079e7d09b395e6d43e78247429bb5a6987e
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-13 11:54:19 -0600

    Add friendships controller to create a new friend and display

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,8 +1,8 @@
 class FriendshipsController < ApplicationController
   def create
-    friend = User.find_by(uid: params[:uid])
-    friendship = Friendship.new(user: current_user, friend: friend)
-    if friendship.save
+    friend = User.find_by(github_username: params[:github_username])
+
+    if current_user.friends << friend
       flash[:success] = 'Successfully added friend'
     else
       flash[:error] = 'Unable to find user'
