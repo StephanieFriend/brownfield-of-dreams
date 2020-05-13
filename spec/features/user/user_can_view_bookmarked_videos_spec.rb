75016b6764ffb43c151e92527264b4c04347dd1d
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-13 16:00:27 -0600

    Update tests to have links and add sad path testing

--- spec/features/user/user_can_view_bookmarked_videos_spec.rb
+++ spec/features/user/user_can_view_bookmarked_videos_spec.rb
@@ -21,13 +21,13 @@ describe 'A registered user' do
     visit '/dashboard'
 
     within ".tutorial-#{tutorial1.title.delete(" ")}" do
-      expect(page).to have_content(video1.title)
-      expect(page).to have_no_content(video2.title)
+      expect(page).to have_link(video1.title)
+      expect(page).to have_no_link(video2.title)
     end
 
     within ".tutorial-#{tutorial2.title.delete(" ")}" do
-      expect(page).to have_content(video2.title)
-      expect(page).to have_no_content(video1.title)
+      expect(page).to have_link(video2.title)
+      expect(page).to have_no_link(video1.title)
     end
   end
 
@@ -47,9 +47,29 @@ describe 'A registered user' do
     visit '/dashboard'
 
     expect(page).to have_content(tutorial1.title)
-    expect(page).to have_content(video1.title)
+    expect(page).to have_link(video1.title)
     expect(page).to have_no_content(tutorial2.title)
-    expect(page).to have_no_content(video2.title)
+    expect(page).to have_no_link(video2.title)
+  end
+
+  it 'can click on title and be redirected to video show page' do
+    tutorial1= create(:tutorial, title: "How to Tie Your Shoes")
+    video1 = create(:video, title: "The Bunny Ears Technique", tutorial: tutorial1)
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit tutorial_path(tutorial1)
+
+    click_on 'Bookmark'
+
+    visit '/dashboard'
+
+    within ".tutorial-#{tutorial1.title.delete(" ")}" do
+      click_link "#{video1.title}"
+    end
+
+    expect(current_path).to eq("/videos/#{video1.id}")
   end
 end
 
