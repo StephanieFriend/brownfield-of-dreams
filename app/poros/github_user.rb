2ce5867083a89ac5fdfd56083f762eead9203529
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-12 22:24:08 -0600

    Add to initialize in github user

--- app/poros/github_user.rb
+++ app/poros/github_user.rb
@@ -1,6 +1,7 @@
 class GithubUser
-  attr_reader :name, :url, :avatar_url
+  attr_reader :name, :url, :avatar_url, :uid
   def initialize(user_details = {})
+    @uid = user_details[:id]
     @name = user_details[:login]
     @url = user_details[:url]
     @avatar = user_details[:avatar_url]
