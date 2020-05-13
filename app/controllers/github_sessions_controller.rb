67821fd0acef1c4ecc961964c5c626521442d8cc
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-13 15:51:33 -0600

    remove unused destroy action

--- app/controllers/github_sessions_controller.rb
+++ app/controllers/github_sessions_controller.rb
@@ -1,12 +1,12 @@
 class GithubSessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    current_user.update!(github_token: auth[:credentials][:token], github_username: auth[:extra][:raw_info][:login])
-    redirect_to root_url, :notice => "Signed in with Github!"
+    if auth 
+      current_user.update(github_token: auth[:credentials][:token], github_username: auth[:extra][:raw_info][:login])
+      redirect_to root_url, :notice => "Signed in with Github!"
+    else
+      redirect_to dashboard_path, :notice => "Failed to connect to Github"
+    end
   end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_url, :notice => "Signed out!"
-  end
 end
